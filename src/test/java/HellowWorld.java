03c9f90c5e9a8bb9c6080cec9491a92f6c8c8bcb
kathik <karthik.pabbathi@optimworks.com>
2025-05-02 02:12:14 +0530

    testNHG added1

--- src/test/java/HellowWorld.java
+++ src/test/java/HellowWorld.java
@@ -1,9 +1,11 @@
+import org.testng.annotations.Test;
 
 public class HellowWorld {
 
-	
+	@Test
 	public static void testRUN () {
 		// TODO Auto-generated method stub
+		System.out.println("HGello worlds");
 		System.out.println("HGello worlds");
 	}
 }
