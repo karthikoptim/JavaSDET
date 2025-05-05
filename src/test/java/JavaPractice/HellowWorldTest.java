39a736178c35ba5ffc6f75eaec3544acf545a5dc
kathik <karthik.pabbathi@optimworks.com>
2025-05-05 18:19:16 +0530

    git hub actions 8

--- src/test/java/JavaPractice/HellowWorldTest.java
+++ src/test/java/JavaPractice/HellowWorldTest.java
@@ -1,9 +1,14 @@
 package JavaPractice;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,11 +20,15 @@ public class HellowWorldTest {
 	 WebDriver driver;
 
 	    @BeforeClass
-	    public void setup() throws InterruptedException {
+	    public void setup() throws InterruptedException, IOException {
 	        // Set ChromeDriver path if not added to system PATH
 	        // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
 
-	        driver = new ChromeDriver();
+	    	Path tempProfile = Files.createTempDirectory("chrome-profile");
+	        ChromeOptions options = new ChromeOptions();
+	        options.addArguments("--user-data-dir=" + tempProfile.toString());
+	        
+	        driver = new ChromeDriver(options);
 	        driver.manage().window().maximize();
 	        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 	        Thread.sleep(10000);
